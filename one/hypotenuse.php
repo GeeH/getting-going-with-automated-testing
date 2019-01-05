79f329809d599da5d3035865e752636b2d66b661
Gary Hockin <geeh@roave.com>
2019-01-05 16:26:55 +0000

    Intermediate step

--- one/hypotenuse.php
+++ one/hypotenuse.php
@@ -3,5 +3,9 @@ declare(strict_types=1);
 
 function hypotenuse($firstSide, float $secondSide): float
 {
-    return sqrt(($firstSide * $firstSide) + ($secondSide + $secondSide));
+    return sqrt($firstSide * $firstSide + $secondSide * $secondSide);
 }
+
+echo hypotenuse(3.0, 4.0); // should be 5
+
+// php -S 0.0.0.0:8080
