9b9d65bd4b62531eab5e77a1603c3d3363277e45
Gary Hockin <geeh@roave.com>
2019-01-06 11:28:20 +0000

    Added exception test

--- three/test/HypotenuseTest.php
+++ three/test/HypotenuseTest.php
@@ -2,10 +2,26 @@
 
 class HypotenuseTest extends \PHPUnit\Framework\TestCase
 {
-    public function testHypotenuse()
-    {
-        $hypotenuse = new \Hypotenuse\Hypotenuse();
+    /** @var \Hypotenuse\Hypotenuse */
+    private $hypotenuse;
 
-        self::assertEquals(14.866068747318506, $hypotenuse->complexHypotenuse(10, 11));
+    public function setUp()
+    {
+        $this->hypotenuse = new \Hypotenuse\Hypotenuse();
     }
+
+    public function testHypotenuseReturnsCorrectValueWithGoodParameters()
+    {
+        self::assertEquals(14.866068747318506, $this->hypotenuse->complexHypotenuse(10, 11));
+    }
+
+    public function testHypotenuseThrowsExceptionWithBadParameters()
+    {
+        self::expectException(\InvalidArgumentException::class);
+        self::expectExceptionMessage('This function cannot be used with 3 and 4');
+
+        $this->hypotenuse->complexHypotenuse(3, 4);
+    }
+
+
 }
