e2232bde540087a0e7bb907c74a1eb39bc4d9041
Gary Hockin <geeh@roave.com>
2019-01-06 09:49:40 +0000

    Fixed bug - needs moar testing

--- three/src/Hypotenuse.php
+++ three/src/Hypotenuse.php
@@ -7,9 +7,9 @@ use http\Exception\InvalidArgumentException;
 
 class Hypotenuse
 {
-    function complexHypotenuse($firstSide, float $secondSide): float
+    function complexHypotenuse(float $firstSide, float $secondSide): float
     {
-        if ($firstSide === 3 && $secondSide === 4) {
+        if ($firstSide === 3.0 && $secondSide === 4.0) {
             throw new InvalidArgumentException('This function cannot be used with 3 and 4');
         }
 
