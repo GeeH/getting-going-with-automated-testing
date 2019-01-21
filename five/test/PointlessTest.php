c603167384da85942b2d97aa306d926f3384222a
Gary Hockin <geeh@roave.com>
2019-01-21 14:10:43 +0000

    Five finished

--- five/test/PointlessTest.php
+++ five/test/PointlessTest.php
@@ -5,18 +5,34 @@ class PointlessTest extends \PHPUnit\Framework\TestCase
     /** @var \Pointless\Pointless */
     private $pointless;
 
+    /** @var \PHPUnit\Framework\MockObject\MockObject|\Monolog\Logger */
+    private $logger;
+
     public function setUp()
     {
-        $this->pointless = new Pointless\Pointless();
+        $this->logger = $this->createMock(\Monolog\Logger::class);
+        $this->pointless = new Pointless\Pointless($this->logger);
     }
 
     public function testIntPassedToPointlessMethodReturnsTrue()
     {
+        $this->logger
+            ->expects(self::once())
+            ->method('info')
+            ->with('$property was int')
+            ->willReturn(true);
+
         self::assertTrue($this->pointless->pointlessMethod(100));
     }
 
     public function testStringPassedToPointlessMethodThrowsException()
     {
+        $this->logger
+            ->expects(self::once())
+            ->method('error')
+            ->with('$property was string')
+            ->willReturn(true);
+
         self::expectException(\RuntimeException::class);
         self::expectExceptionMessage('No strings are allowed');
 
@@ -25,11 +41,23 @@ class PointlessTest extends \PHPUnit\Framework\TestCase
 
     public function testBoolPassedToPointlessMethodReturnsFalse()
     {
+        $this->logger
+            ->expects(self::once())
+            ->method('info')
+            ->with('$property was bool')
+            ->willReturn(true);
+
         self::assertFalse($this->pointless->pointlessMethod(false));
     }
 
     public function testArrayPassedToPointlessMethodThrowsException()
     {
+        $this->logger
+            ->expects(self::once())
+            ->method('error')
+            ->with('$property was invalid')
+            ->willReturn(true);
+
         self::expectException(\InvalidArgumentException::class);
         self::expectExceptionMessage('No valid property passed');
 
