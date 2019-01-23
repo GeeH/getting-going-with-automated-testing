45201ffb49291fd0f104c440dcc4d2926fb43bc1
Gary Hockin <geeh@roave.com>
2019-01-23 10:57:32 +0000

    Six End

--- six/test/ControllerTest.php
+++ six/test/ControllerTest.php
@@ -2,8 +2,8 @@
 
 namespace Test\App;
 
-
 use App\Controller;
+use App\User;
 use App\UserService;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
@@ -24,6 +24,33 @@ class ControllerTest extends TestCase
 
     public function testInvokeReturnsArrayWhenUserIdSupplied()
     {
+        $user = new User();
+        $user->id = 42;
+        $user->name = 'Zaphod';
+
+        $this->userServiceMock
+            ->expects($this->once())
+            ->method('getUserById')
+            ->with(42)
+            ->willReturn($user);
+
+        $this->userServiceMock
+            ->expects($this->once())
+            ->method('isUserAdmin')
+            ->with($user)
+            ->willReturn(true);
+
+        $result = $this->controller->__invoke(42);
+
+        self::assertSame($result[0], $user);
+        self::assertTrue($result[1]);
     }
 
+    public function testInvokeThrowsExceptionIfUserIdIsZero()
+    {
+        $this->expectExceptionCode(404);
+        $this->expectExceptionMessage('We do not use zeroes here');
+
+        $this->controller->__invoke(0);
+    }
 }
